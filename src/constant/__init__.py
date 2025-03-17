e9f1cce4ae22497623440aa5493a223f31aec2d7
Shashank Anand <shashankanandsa1@gmail.com>
2025-03-18 01:59:56 +0530

    aaded models & schema

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -10,4 +10,4 @@ MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
 
 artifact_folder_name = datetime.now().strftime('%m_%d_%Y_%H_%M_%S')
-artifact_folder = os.path.join("artifacts", artifact_folder_name)
+artifact_folder = os.path.join("artifacts", artifact_folder_name)   
